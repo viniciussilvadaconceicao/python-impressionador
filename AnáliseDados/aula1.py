e4762c00e243e6977ea6433728b5a92868465ff0
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-13 23:35:57 -0300

    Create aula1.py

---
+++ AnáliseDados/aula1.py
@@ -0,0 +1,34 @@
+'''os 6 passsos de uma Análise de Dados
+
+1 identificação de problemas e objetivo de análise'''
+#simplifique 1 número, 1 problema, 1 desafio
+
+'''2 princípio 80 / 20'''
+#20% das causas geram 80% dos efeitos
+
+'''3 causa e efeito dos principais ofensores e oportunidades'''
+#entender o impacto dos 20% sobre o problema original
+'''4 visualização de dados e comparação'''
+
+'''5 Questione e duvide - Um framework para criar hipóteses'''
+
+'''6 confime suas teorias'''
+
+
+#aplicação em case 
+'''voce foi contratado para reduzir oos custos de uma fábrica da cocacola
+1 problema custo total da fabrica no ano
+
+quais os principais custos?
+energia: 300.000
+açucar: 5.000.000
+plastico: 4.000.000
+agua: 250.000
+mão de obra: 1.000.000
+manutenção: 1.250.000
+outros: 500.000
+
+acucar + plastico = 9.000.000
+custo total = 12.300.000
+2 itens = 73% do custo total
+'''
