d2e64c2bdf61c501c0e2e78807e5a519967e30ec
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-14 18:43:23 -0300

    Update gerenciarVendas.py

--- python/gerenciarVendas.py
+++ python/gerenciarVendas.py
@@ -1,34 +1,44 @@
-'''vendedores = {'João':100, 'Maria':110, 'José':120, 'Ana':125, 'Carlos': 150, 'Pedro': 200, 'Paulo':250,  'Lucas':300,}
-for nome in vendedores:
-    print(f'vendedor: {nome}, vendeu: {vendedores[nome]}')'''
-#se a loja tiver um numero especifico de vendedores usamos o dicionario
-meta = 130
-nao_bateu = []
-bateu = []
-cont = 2
-print('\n')
-print('='*80)      
-print('''Bem vindo ao sistema de vendas:
-Digite o nome do vendedor e o valor de suas vendas para saber se ele bateu a meta''')
-print('='*80)      
-for i in range(cont):
-    funcionarios = dict()
-    funcionarios['vendedor'] = str(input('Nome: ')).upper().strip()
-    funcionarios['vendas']= float(input('Vendas: '))
-    if funcionarios['vendas'] < meta:
-        nao_bateu.append(funcionarios)
-    elif funcionarios['vendas'] >= meta:
-        bateu.append(funcionarios)
-print('\n')        
-print('='*44)
-print('Os vendedores que não bateram a meta foram:')  
-print('='*44)      
-for i in nao_bateu:
-    print(f'{i["vendedor"]}')
-print('\n')
-print('='*44)
-print('Os vendedores que BATERAM A META!:')  
-print('='*44)
-for i in bateu:
-    print(f'{i["vendedor"]}')
-print('\n')
+def gerenciarVendas():
+    meta = 130
+    nao_bateu = []
+    bateu = []
+    print('-'*80)
+    print('''
+Primeiro quantos vendedores iram participar da bonificação
+          
+ATENÇÃO: SERÃO ACEITOS SOMENTE NÚMEROS INTEIROS SEM PONTO OU VÍRGULA
+          ''')
+    print('-'*80)
+    cont = int(input('Quantos vendedores deseja cadastrar: '))
+    print('\n')
+    print('='*85)      
+    print('''Bem vindo ao sistema de vendas.
+Primeiro digite o nome do vendedor e confirme, logo após o programa irá perguntar 
+o valor total de vendas desse vendedor.''')
+    print('='*85)      
+    for i in range(cont):
+        funcionarios = dict()
+        funcionarios['vendedor'] = str(input('Vendedor: ')).upper().strip()
+        funcionarios['vendas']= float(input('Valor de vendas: '))
+        print('\n')
+        if funcionarios['vendas'] < meta:
+            nao_bateu.append(funcionarios)
+            print('='*44)
+            print('NÃO BATERAM A META:')  
+            print('='*44)      
+            for i in nao_bateu:
+                print(f'{i["vendedor"]}')
+            print('\n')
+            print('-'*44)
+            
+        elif funcionarios['vendas'] >= meta:
+            bateu.append(funcionarios)
+            print('='*44)
+            print('BATERAM A META!:')  
+            print('='*44)
+            for i in bateu:
+                print(f'{i["vendedor"]}')
+            print('\n')
+            print('-'*44)
+
+gerenciarVendas()
