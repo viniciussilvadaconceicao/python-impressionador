5168c5b31994dcc8ba0d51f26fa392c470ff68ea
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-14 16:43:29 -0300

    Update carrinho..py

--- python/carrinho..py
+++ python/carrinho..py
@@ -2,7 +2,7 @@
 def carrinho():    
     produto = []
     print('=='*30)
-    print('Digite FIM caso queira encerrar. ')
+    print('Adicione produtos no carinho, caso queira encerrar escreva fim. ')
     print('-'*30)
     inserir_produtos = str(input('Digite o nome do produto: ')).upper()
     print('-'*30)
@@ -18,4 +18,12 @@ def carrinho():
     print('-'*30)
     print()
 
-carrinho()
+
+vendas = [100,110,120,125, 150, 200, 250, 300]
+vendedores = ['João', 'Maria', 'José', 'Ana', 'Carlos', 'Pedro', 'Paulo', 'Lucas']
+meta = 130
+nao_bateu = [] 
+for venda in vendas:
+    if venda < meta:
+       nao_bateu.append(venda)
+print(f'Vendedores que não bateram a meta: {nao_bateu}')
