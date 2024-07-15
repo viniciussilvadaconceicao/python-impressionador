a1fef2b26c6aac1a1306cf8123f5d512fbfc6d26
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-15 12:30:43 -0300

    Update funçãoParametros.py

--- python/funçãoParametros.py
+++ python/funçãoParametros.py
@@ -1,14 +1,16 @@
-def alcoolico(bebidas):
+def eh_da_categoria(bebidas, categoria):
     bebidas = bebidas.upper()
-    if 'BEB' in bebidas:
+    if categoria in bebidas:
         return True
     else:
         return False
 
 
 
-produtos = ['beb1290', 'BEB2340', 'beb5670', 'BEB6780', 'beb8900', 'BEB3030', 'beb4040', 'BEB5050', 'beb6060', 'BEB7070','TAF2112', 'BAB2111']
+produtos = ['beb1290', 'BEB2340', 'beb5670', 'BEB6780', 'beb8900', 'BEB3030', 'beb4040', 'BEB5050', 'beb6060', 'BEB7070','bab2112', 'BAB2111']
 
 for produto in produtos:
-    if alcoolico(produto):
+    if eh_da_categoria(produto, 'BEB'):
         print(f'enviar o {produto} para a seção de bebidas alcoólicas')
+    elif eh_da_categoria(produto,'BAB'):
+        print(f'enviar o {produto} para a seção de bebidas não alcoólicas')
