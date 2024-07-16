37f0c6603c88534048e2ad6c30a564a91be880be
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-16 19:16:01 -0300

    Update argumentos.py

--- python/argumentos.py
+++ python/argumentos.py
@@ -6,7 +6,22 @@ def somatorio(*args):
         soma += numero
     return soma
 
-print(somatorio(1,4,5,10,100))
+#print(somatorio(1,4,5,10,100))
 
 # (*args) é usado para passar um número variável de argumentos muitos dão o nome de args
 #(**kwargs) é usado para passar como dict um número variável de argumentos muitos dão o nome de kwargs
+
+def preco_final(preco, **kwargs):
+    '''calcula o preço final de um produto'''
+    print(kwargs)#para ver o que está sendo passado em dict
+    if 'desconto' in kwargs:
+        preco = preco - kwargs['desconto']
+    if 'garantia_extra' in kwargs:
+        preco = preco + kwargs['garantia_extra']
+    if 'frete' in kwargs:
+        preco = preco + kwargs['frete']
+    if 'imposto' in kwargs:
+        preco = preco + kwargs['imposto']
+        return preco
+
+print(preco_final(2000, desconto = 0.5, garantia_extra = 100, frete = 50, imposto = 0.1))
