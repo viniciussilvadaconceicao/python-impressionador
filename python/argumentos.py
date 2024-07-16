00a0d15ac5eb0cd95a850922d644fd6ac138d64a
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-16 18:52:44 -0300

    Update argumentos.py

--- python/argumentos.py
+++ python/argumentos.py
@@ -7,3 +7,6 @@ def somatorio(*args):
     return soma
 
 print(somatorio(1,4,5,10,100))
+
+# (*args) é usado para passar um número variável de argumentos muitos dão o nome de args
+#(**kwargs) é usado para passar como dict um número variável de argumentos muitos dão o nome de kwargs
