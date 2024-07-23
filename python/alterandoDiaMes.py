17932388ab226d7338048d3298b84632a30e3b84
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-23 19:20:05 -0300

    Update alterandoDiaMes.py

--- python/alterandoDiaMes.py
+++ python/alterandoDiaMes.py
@@ -1,3 +1,5 @@
+import time
+
 tempo_BR = "23/07/2024"
 formato = "%d/%M/%Y"
 print(tempo_BR)
@@ -5,3 +7,9 @@ print(tempo_BR)
 tempo_EUA = '07/23/2024'
 formato = '%M/%d/%Y'
 print(tempo_EUA)
+
+struk_tempo = time.localtime()#aqui ele pega o tempo local
+print(struk_tempo)
+
+gmt_exemplo = time.gmtime()#aqui ele pega o tempo de greenwich
+print(gmt_exemplo)
