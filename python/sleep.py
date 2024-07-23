015f55cc112fe555e34e4aba6ac31648e8152f12
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-23 19:29:59 -0300

    Update sleep.py

--- python/sleep.py
+++ python/sleep.py
@@ -1,6 +1,6 @@
 import time
 
 for cont in range (10,0,-1):
-    print(cont ,end=' \r')
+    print(cont ,end=' \r')# o \r faz com que o cursor volte para o inicio da linha
     time.sleep(1)
 print('Feliz ano novo!!!')
