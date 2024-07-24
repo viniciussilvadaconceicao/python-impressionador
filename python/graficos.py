8b6f5dff5f827384c1f5b1c2c4f39dd11b40699b
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-24 20:51:18 -0300

    Update graficos.py

--- python/graficos.py
+++ python/graficos.py
@@ -2,7 +2,7 @@ vendas_meses = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 10000, 7000, 800
 meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
 #plotar um gráfico de forma simples
 import matplotlib.pyplot as plt 
-plt.plot(vendas_meses)
+plt.plot(meses, vendas_meses)
 plt.ylabel('Vendas')
 plt.xlabel('Meses')
 plt.show()
