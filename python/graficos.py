aad5de1d9c7e69da1f510d87e199af2161aac8db
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-24 20:49:25 -0300

    Create graficos.py

---
+++ python/graficos.py
@@ -0,0 +1,8 @@
+vendas_meses = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 10000, 7000, 8000, 4000]
+meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+#plotar um gráfico de forma simples
+import matplotlib.pyplot as plt 
+plt.plot(vendas_meses)
+plt.ylabel('Vendas')
+plt.xlabel('Meses')
+plt.show()
