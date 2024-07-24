74ae0e6fc3e4df10394f3595def3cc950d9d5e56
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-24 20:33:42 -0300

    Create modulos.py

---
+++ python/modulos.py
@@ -0,0 +1,2 @@
+from collections import counter
+vendas_tecnologias= {'notebooks':3000, 'desktops':2000, 'smartphones':5000, 'tablets':1000 ,'sansung':25000, 'apple':30000, 'motorola':15000, 'lg':10000}
