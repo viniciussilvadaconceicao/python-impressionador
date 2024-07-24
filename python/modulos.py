9bc66231f1e5c44d053980465fdca951509f9c9f
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-24 20:37:38 -0300

    Update modulos.py

--- python/modulos.py
+++ python/modulos.py
@@ -1,5 +1,5 @@
-from collections import Counter
+from collections import Counter #Counter é usado para contar a quantidade de elementos em uma lista serve para dict, list, str, tuple, collections, etc
 vendas_tecnologias= {'notebooks':3000, 'desktops':2000, 'smartphones':5000, 'tablets':1000 ,'sansung':25000, 'apple':30000, 'motorola':15000, 'lg':10000}
 
 auxiliar = Counter(vendas_tecnologias)
-print(auxiliar.most_common(3))
+print(auxiliar.most_common(3))#most_common é usado para mostrar os 3 elementos mais comuns da lista
