c2ee3c925ab8a3d3785d491be3e6906aa9ccffe4
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-24 20:36:12 -0300

    Update modulos.py

--- python/modulos.py
+++ python/modulos.py
@@ -1,2 +1,5 @@
-from collections import counter
+from collections import Counter
 vendas_tecnologias= {'notebooks':3000, 'desktops':2000, 'smartphones':5000, 'tablets':1000 ,'sansung':25000, 'apple':30000, 'motorola':15000, 'lg':10000}
+
+auxiliar = Counter(vendas_tecnologias)
+print(auxiliar.most_common(3))
