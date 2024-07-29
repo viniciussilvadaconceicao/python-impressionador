0955d07ed859e45100b54ee8cc9c659d3401da2f
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-29 19:04:00 -0300

    Update 2graficos.py

--- python/2graficos.py
+++ python/2graficos.py
@@ -2,3 +2,7 @@
 import matplotlib.pyplot as plt
 #importando o numpy
 import numpy as np
+# o random gera valores aleatórios, randint gera valores inteiros aleatórios
+venda = np.random.randint(1000,3000,50)
+#arenge gera um vetor de 1 a 50
+meses = np.arange(1,51)
