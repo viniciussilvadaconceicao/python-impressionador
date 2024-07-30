0343280379a4f8c02639689458acc7796da1b117
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-30 18:34:50 -0300

    Create ex001.py

---
+++ exercicios/ex001.py
@@ -0,0 +1,18 @@
+faturamento = {
+    'janeiro': 'R$95000',
+    'fevereiro': 'R$98000',
+    'março': 'R$101000',
+    'abril': 'R$100000',
+    'maio': 'R$98000',
+    'junho': 'R$95000',
+    'julho': 'R$98000',
+    'agosto': 'R$101000',
+    'setembro': 'R$100000',
+    'outubro': 'R$98000',
+    'novembro': 'R$95000',
+    'dezembro': 'R$71000'
+}
+#voce precisa inserir no sistema um dicionário no formato :
+'''resultado = {
+'mes':(faturamento, imposto_mensal, imposto_trimetral)
+}'''
