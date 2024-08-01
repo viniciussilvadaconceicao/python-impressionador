346b0a8ba8469997248c075c33f81702e9df032f
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-01 19:55:44 -0300

    Update geradorSenha.py

--- exercicios/geradorSenha.py
+++ exercicios/geradorSenha.py
@@ -1,7 +1,14 @@
 def gerador_senha(tamanho):
     if tamanho < 4:
         print("Senha muito curta")
-    
+    else:
+        import random
+        import string
+        senha = [
+            random.choice(string.ascii_letters),
+            random.choice(string.digits),
+            random.choice(string.punctuation)
+        ]
 
 
 tamanho = int(input("Digite o tamanho da senha: "))
