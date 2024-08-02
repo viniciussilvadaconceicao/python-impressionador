5315ef9cc6c042a6df61790ec1e786469aec85ff
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-02 18:47:17 -0300

    caminho.py

--- exercicios/QrcodeImagem.py
+++ exercicios/QrcodeImagem.py
@@ -1,9 +1,13 @@
 import qrcode
 from qrcode.image.styledpil import StyledPilImage #Pilimage é uma classe que representa uma imagem
 
-qr = qrcode.QRCode()
-qr.add_data("https://api.whatsapp.com/send?phone=5522992240688")
-qr.make(fit=True)
+qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)  # para poder adicionar uma imagem
+qr.add_data("https://www.youtube.com/@HashtagProgramacao")
 
-imagem = qr.make_image(image_factory=StyledPilImage, embeded_image_path="python.png")
+imagem = qr.make_image(
+    image_factory=StyledPilImage,
+    embeded_image_path = "C:\\Users\\horti\\OneDrive\\Documentos\\meusProjetos\\Portf-lio\\imagens\\python.jpg"
+,
+)
+
 imagem.save("qrcode_logo.png")
