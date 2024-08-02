3f77c85dc3673c4f512c2d486b53cf751af24542
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-02 18:22:24 -0300

    Update Qrcode.py

---
+++ exercicios/Qrcode.py
@@ -0,0 +1,5 @@
+#primeiro instalar pip install qrcode
+#segundo instalar pip install Pillow
+import qrcode
+#crio uma variavel para receber o qrcode
+imagem = qrcode.make("https://api.whatsapp.com/send?phone=5522992240688")
