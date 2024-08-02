46f0bc31146827ce6075de98c7e04c430922c4b7
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-02 18:42:33 -0300

    Update QrcodeImagem.py

--- exercicios/QrcodeImagem.py
+++ exercicios/QrcodeImagem.py
@@ -1,2 +1,9 @@
 import qrcode
-from qrcode.image.pil import PilImage #Pilimage é uma classe que representa uma imagem
+from qrcode.image.styledpil import StyledPilImage #Pilimage é uma classe que representa uma imagem
+
+qr = qrcode.QRCode()
+qr.add_data("https://api.whatsapp.com/send?phone=5522992240688")
+qr.make(fit=True)
+
+imagem = qr.make_image(image_factory=StyledPilImage, embeded_image_path="python.png")
+imagem.save("qrcode_logo.png")
