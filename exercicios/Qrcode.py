a3de4e3d4fafc695bde3e4ea741a7cb46f9903a4
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-02 18:28:51 -0300

    QrcodeImagem.py

--- exercicios/Qrcode.py
+++ exercicios/Qrcode.py
@@ -1,5 +1,6 @@
 #primeiro instalar pip install qrcode
 #segundo instalar pip install Pillow
 import qrcode
-#crio uma variavel para receber o qrcode
-imagem = qrcode.make("https://api.whatsapp.com/send?phone=5522992240688")
+
+imagem = qrcode.make("https://api.whatsapp.com/send?phone=5522992240688")#crio uma variavel para receber o qrcode
+imagem.save("qrcode.png")#salvo o qrcode em um arquivo png
---
+++ exercicios/QrcodeImagem.py
@@ -0,0 +1,2 @@
+import qrcode
+from qrcode.image.pil import PilImage #Pilimage é uma classe que representa uma imagem
