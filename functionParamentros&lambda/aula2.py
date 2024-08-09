b564e9f2f455d97ee450ad3805657549b7a7b63c
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-09 19:23:20 -0300

    Update aula2.py

--- functionParamentros&lambda/aula2.py
+++ functionParamentros&lambda/aula2.py
@@ -1,3 +1,10 @@
 produtos = ['Iphone 11','Ipad','Iphone 15', 'airpods','macbook','apple watch','apple tv','mac mini','mac pro','mac air']
 produtos.sort(key= str.casefold)
 print(produtos)	
+
+vendas_produtos = {'iphone':400,'ipad':550,'sansung':1000,'pc':18000}
+
+def segundo_elemento(tupla):
+    return tupla[1]
+
+vendas_produtos = list(vendas_produtos.items())
