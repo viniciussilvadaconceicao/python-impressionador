bb441027306290f9abe8a5703f716120776aca48
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-11 19:27:43 -0300

    Update aula3.py

--- functionParamentros&lambda/aula3.py
+++ functionParamentros&lambda/aula3.py
@@ -8,3 +8,4 @@ def minha_função2(num):
     '''função que retorna o dobro'''
     return num * 2
 print(minha_função2(5))
+# a função acima é a mesma coisa que a de cima, só que com o lambda é mais simples
