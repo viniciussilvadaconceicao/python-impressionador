9e8315bd236682373b9eb99f1d7d6f3a709ef23a
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-11 19:57:44 -0300

    Update aula4.py

--- functionParamentros&lambda/aula4.py
+++ functionParamentros&lambda/aula4.py
@@ -4,7 +4,7 @@ def imposto(valor):
     '''Função que calcula o imposto de um produto'''
     return valor * (1 + 0.3)
 
-preco_imposto = list(map(imposto,preco_tecnologicos.values()))#map(função, lista)
+preco_imposto = list(map(imposto,preco_tecnologicos.values()))#map(função, lista) ele pega a função e aplica na lista	
 print(preco_imposto)
 print('\n')
 
