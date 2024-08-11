d27e7287a253a52d9aad15a390fd610ce7737835
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-11 19:27:19 -0300

    Update aula3.py

--- functionParamentros&lambda/aula3.py
+++ functionParamentros&lambda/aula3.py
@@ -1,2 +1,10 @@
 '''estrutura
 minha_função = lambda parametros: expressão'''
+minha_função = lambda num : num * 2
+print(minha_função(5))
+# o lambda é uma função anonima, que não precisa de um nome para ser chamada
+
+def minha_função2(num):
+    '''função que retorna o dobro'''
+    return num * 2
+print(minha_função2(5))
