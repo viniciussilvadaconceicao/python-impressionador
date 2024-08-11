4a9147975e67231f0da1e1cd4d17b36f17c34889
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-11 19:51:44 -0300

    Update aula4.py

---
+++ functionParamentros&lambda/aula4.py
@@ -0,0 +1,8 @@
+preco_tecnologicos = {'asus': 4000, 'samsung': 7000, 'apple': 6000 , 'xaomi':5000}
+
+def imposto(valor):
+    '''Função que calcula o imposto de um produto'''
+    return valor * (1 + 0.3)
+
+preco_imposto = list(map(imposto,preco_tecnologicos.values()))
+print(preco_imposto)
