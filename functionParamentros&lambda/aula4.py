36c94b43e66a22b65aaa20a9c80f55b6fec84da7
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-11 20:20:02 -0300

    Update aula4.py

--- functionParamentros&lambda/aula4.py
+++ functionParamentros&lambda/aula4.py
@@ -11,12 +11,17 @@ print('\n')
 # Função lambda
 preco_imposto2 = list(map(lambda valor: valor * ( 1 + 0.3) ,preco_tecnologicos.values()))#map(lambda, lista)
 print(preco_imposto2)
+print('\n')
 
 #agora queremos apenas os valores que são maiores que 4000
-
 def maiorque4000(item):
     '''função que verifica se o valor é maior que 4000'''
     return item[1] > 4000
 
-produtos_maiorque4000 = list(filter(maiorque4000, preco_tecnologicos.items()))
+produtos_maiorque4000 = dict(filter(maiorque4000, preco_tecnologicos.items()))#filter(função, dicionario.items()) ele pega a função e aplica no dicionario
 print(produtos_maiorque4000)
+print('\n')
+
+# função lambda
+produtos_maiorque3900 = dict(filter(lambda item: item[1]> 3900, preco_tecnologicos.items()))
+print(produtos_maiorque3900)
