02b115ec4804ffcad628ac5ad16dfe1e9413f512
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-11 19:55:35 -0300

    Update aula4.py

--- functionParamentros&lambda/aula4.py
+++ functionParamentros&lambda/aula4.py
@@ -6,3 +6,6 @@ def imposto(valor):
 
 preco_imposto = list(map(imposto,preco_tecnologicos.values()))
 print(preco_imposto)
+
+# Função lambda
+preco_imposto2 = list(map(lambda valor: valor * ( 1 + 0.3) ,preco_tecnologicos.values()))
