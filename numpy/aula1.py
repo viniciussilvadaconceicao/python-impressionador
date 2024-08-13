25d7b6c88aabc0e26c6c0cfea173421dd02c6385
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-13 20:07:01 -0300

    Update aula1.py

--- numpy/aula1.py
+++ numpy/aula1.py
@@ -8,9 +8,17 @@ def arry():
     print(lista)
     for elemento in lista:
         print(type(elemento))
+def soma_lista():
+    '''somando na lista'''
+    lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    nova_lista = []
+    for num in lista:
+        nova_lista.append(num + 1)
+    print(nova_lista)
 
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-nova_lista = []
-for num in lista:
-    nova_lista.append(num + 1)
-print(nova_lista)
+def somando_arry():
+    '''somando arrays'''
+    arry = np.array([1,2, 3, 4, 5, 6, 7, 8, 9, 10])
+    arry_soma = arry + 1 
+    print(arry_soma)
+print(somando_arry())
