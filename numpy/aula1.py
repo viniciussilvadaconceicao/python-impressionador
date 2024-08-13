b2186edb0d9b8aca5db8bb35e534700e28fbc25b
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-13 19:45:15 -0300

    Update aula1.py

--- numpy/aula1.py
+++ numpy/aula1.py
@@ -1,4 +1,9 @@
 import numpy as np 
 
 arry = np.array([1,2, 3, 4, 5, 6, 7, 8, 9, 10])
+#arry 
+lista = [1, "dois", 3.0]
 print(arry)
+print(lista)
+for elemento in lista:
+    print(type(elemento))
